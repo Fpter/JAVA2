75558dc931ca24e21df485f427d57f7c16395360
quanganhsaker <quanganhsaker@gmail.com>
2022-07-27 20:07:52 +0700

    edit func

--- DemoTable/src/main/java/com/mycompany/demotable/PersonFrame.java
+++ DemoTable/src/main/java/com/mycompany/demotable/PersonFrame.java
@@ -51,6 +51,7 @@ public class PersonFrame extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tblInfo = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -107,6 +108,13 @@ public class PersonFrame extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Edit");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -133,7 +141,9 @@ public class PersonFrame extends javax.swing.JFrame {
                             .addComponent(txtAge)
                             .addComponent(txtHeight, javax.swing.GroupLayout.DEFAULT_SIZE, 272, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(132, 132, 132)
+                        .addGap(23, 23, 23)
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btnAdd)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1))
@@ -164,7 +174,8 @@ public class PersonFrame extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnAdd)
-                    .addComponent(jButton1))
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 404, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -247,7 +258,15 @@ public class PersonFrame extends javax.swing.JFrame {
         
         JOptionPane.showMessageDialog(rootPane, selectedRow);
     }//GEN-LAST:event_tblInfoMouseClicked
-
+    void loadData() {
+        model.setRowCount(0);
+        for(int i=0;i<listPerson.size();i++) {
+            Person pi = listPerson.get(i);
+            model.addRow(new Object[] {
+                pi.getName(), pi.getAge(), pi.getHeight()
+            });
+        }
+    }
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         
@@ -261,16 +280,31 @@ public class PersonFrame extends javax.swing.JFrame {
         listPerson.remove(selectedRow); // remove(-1)
         
         // b3: load data tu listPerson len table
-        model.setRowCount(0);
-        for(int i=0;i<listPerson.size();i++) {
-            Person pi = listPerson.get(i);
-            model.addRow(new Object[] {
-                pi.getName(), pi.getAge(), pi.getHeight()
-            });
-        }
+        loadData();
         
         
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        // lay ra index cua dong minh chon
+        int selectedRow = tblInfo.getSelectedRow();
+        // lay ra peson theo index
+        Person selectedPerson = listPerson.get(selectedRow);
+        
+        // sau khi bam edit thì cạp nhat person theo thong tin tu cac textfield
+        String name = txtName.getText();
+        int age = Integer.parseInt(txtAge.getText());
+        double height = Double.parseDouble(txtHeight.getText());
+        
+        selectedPerson.setName(name);
+        selectedPerson.setAge(age);
+        selectedPerson.setHeight(height);
+        JOptionPane.showMessageDialog(rootPane, "Edit thanh cong");
+        // load toan bo person trong list len table
+        loadData();
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -310,6 +344,7 @@ public class PersonFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdd;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
